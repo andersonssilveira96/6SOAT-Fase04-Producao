d0ec4cd9d3dc856646c165bd56cdda6f82373df0
Anderson Souza <andersonssilveira96@gmail.com>
2024-12-01 13:18:37 -0300

    Change rabbitMq

--- src/Infra.MessageBroker/MessageBrokerProducer.cs
+++ src/Infra.MessageBroker/MessageBrokerProducer.cs
@@ -12,7 +12,7 @@ namespace Infra.MessageBroker
         {
             var factory = new ConnectionFactory
             {
-                HostName = "localhost"
+                HostName = "rabbitmq-service"
             };
 
             var connection = await factory.CreateConnectionAsync();
